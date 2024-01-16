39f1630ce480d2683d7322b65f95e29470d07aac
luc <pololuc01@gmail.com>
2024-01-16 15:11:31 +0100

    Creation and implementation of expected_goals()

--- src/features/make_new_features.py
+++ src/features/make_new_features.py
@@ -788,3 +788,28 @@ def possession(dico_col_rk_0, dataset_0):
     dataset_0["AT_Diff_avg_possession"] = (dataset_0["AT_possession"]/(dataset_0["AT_played_matchs_nb"].apply(useful_functions.un_ou_x))) - (dataset_0["HT_possession"]/(dataset_0["HT_played_matchs_nb"].apply(useful_functions.un_ou_x)))
     
     return dataset_0
+
+
+#EXPECTED GOALS / XG (pm, sbos)
+#VARIABLE                    V
+#PER MATCH AVG               V
+#PER MATCH AVG HT/AT DIFF    V
+def expected_goals(dico_col_rk_0, dataset_0):
+    
+    #We ensure that the columns are defined as float columns (because i had an error that raised saying that in was a int64 col)
+    dataset_0["HT_xg"] = dataset_0["HT_xg"].astype(float)
+    dataset_0["AT_xg"] = dataset_0["AT_xg"].astype(float)
+    
+    #On remplit les colonnes "HT_xg", "AT_xg" 
+    useful_functions.variable_sum_computing(dico_col_rk_0['rg_HTXG'], 42, 1,dataset_0)
+        
+        
+    #PER MATCH AVG
+    dataset_0["HT_avg_xg"] = dataset_0["HT_xg"]/(dataset_0["HT_played_matchs_nb"].apply(useful_functions.un_ou_x))
+    dataset_0["AT_avg_xg"] = dataset_0["AT_xg"]/(dataset_0["AT_played_matchs_nb"].apply(useful_functions.un_ou_x))
+        
+    #PER MATCH AVG HT/AT DIFF
+    dataset_0["HT_Diff_avg_xg"] = (dataset_0["HT_xg"]/(dataset_0["HT_played_matchs_nb"].apply(useful_functions.un_ou_x))) - (dataset_0["AT_xg"]/(dataset_0["AT_played_matchs_nb"].apply(useful_functions.un_ou_x)))
+    dataset_0["AT_Diff_avg_xg"] = (dataset_0["AT_xg"]/(dataset_0["AT_played_matchs_nb"].apply(useful_functions.un_ou_x))) - (dataset_0["HT_xg"]/(dataset_0["HT_played_matchs_nb"].apply(useful_functions.un_ou_x)))
+    
+    return dataset_0
