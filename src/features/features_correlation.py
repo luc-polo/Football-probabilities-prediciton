a2671a84b284093b249c79467a65e6e0521eb78b
luc <pololuc01@gmail.com>
2024-01-17 15:22:12 +0100

    Creation and implementation of calculcate_feature_correlation() and feature_correlation_with_results_analysis()

--- src/features/features_correlation.py
+++ src/features/features_correlation.py
@@ -3,7 +3,8 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from scipy.stats import pointbiserialr
+import seaborn as sns
 
 
 #Ploting the mean of a feature at every championship day
@@ -60,12 +61,32 @@ def plot_feature_stats_over_game_weeks(home_feature_column_name, away_feature_co
     
 #Assessing the CORRELATION between two features
 def calculcate_feature_correlation( home_feature_column_name, away_feature_column_name, k, min_week_game, date_min, dataset_0):
+    """  
+    This function computes the correlation of biserial point between a specific feature and the matchs' results, applying some filters to select matchs used to compute our result.
+    
+    Args:
+        home_feature_column_name (str): Name of the column of the home feature we want to compute the correlation of biserial point
+
+        away_feature_column_name (str): Name of the column of the away feature we want to compute the correlation of biserial point
+        
+        k (float): Value used to compute the limits of outliers. We will multiply the IQR by k to compute lower and upper bound for outliers. If we don't want outliers elimination we just need to put a big value for k (like 999).
+        
+        min_week_game (int): The min Game Week of the match we will select to compte the correlation.
+        
+        date_min (datetime): If we want to select only the matchs that played out after a certain date, refer the date with this parameter.
+        
+        dataset_0 (DataFrame): The dataset containing the data.
+    
+    Returns:
+        Tuple: Correlation value, p-value, Mean of the feature when Result = 1, Mean of the feature when Result = 0, df_without_outliers
+    """
     
     #Si on a décidé d'enlever les lignes avant une certaine date:
-    dataset_01= dataset_0[dataset_0["date_GMT"]>date_min]
+    
+    dataset_01 = dataset_0[dataset_0["date_GMT"]>date_min]
     
     #On supprime les lignes qui correspondent à des matchs de journée de championnat < min_week_game:
-    dataset_01 = dataset_01[dataset_0["Game Week"]>= min_week_game]
+    dataset_01 = dataset_01[dataset_01["Game Week"]>= min_week_game]
     
     # Concaténer les colonnes
     Home_Away_Features = pd.concat([dataset_01[home_feature_column_name], dataset_01[away_feature_column_name]], axis=0, ignore_index=True)
@@ -73,7 +94,7 @@ def calculcate_feature_correlation( home_feature_column_name, away_feature_colum
     concatenated_df = pd.DataFrame({'Home_Away_Features': Home_Away_Features,'RH_RA': RH_RA})
 
     #On supprime les outliers en calculant les bornes au dela desquelles on considèrent une valeur comme outlier
-    #Pour se faire une utilise les quartiless
+    #Pour se faire on utilise les quartiless
     Q1 = concatenated_df["Home_Away_Features"].quantile(0.25)
     Q3 = concatenated_df["Home_Away_Features"].quantile(0.75)
     IQR = Q3 - Q1
@@ -81,8 +102,6 @@ def calculcate_feature_correlation( home_feature_column_name, away_feature_colum
     upper_bound = Q3 + k * IQR
     #On supprime les valeurs au dela et en deca des bornes définis à l'aide des quartiles
     df_without_outliers = concatenated_df[(concatenated_df["Home_Away_Features"] >= lower_bound) & (concatenated_df["Home_Away_Features"] <= upper_bound)]
-    
-    
     
     # Calcul de la corrélation entre RH_RA et Home_Away_Feature
     # Calcul de la corrélation de point bisérial spécialement adaptée au cas: variable binaire (1/0) et une variable continue
@@ -95,12 +114,30 @@ def calculcate_feature_correlation( home_feature_column_name, away_feature_colum
     return correlation, p_value, mean_ra1, mean_ra0, df_without_outliers
 
 
-def feature_correlation_with_results_analysis(dataset_0, home_feature_column_name, away_feature_column_name, k, min_week_game, date_min):    
+def feature_correlation_with_results_analysis( home_feature_column_name, away_feature_column_name, k, min_week_game, date_min, dataset_0):    
+    """  
+    This function computes the correlation of biserial point between a specific feature and the matchs' results, applying some filters to select matchs used to compute our result. Other statistics are also displayed: Avg of the feature when Result = 1 and 0. As well as p value.
     
-    correlation, p_value, mean_ra1, mean_ra0, df_without_outliers_0 = calculcate_feature_correlation(dataset_0, home_feature_column_name, away_feature_column_name, k, min_week_game, date_min)
+    Args:
+        home_feature_column_name (str): Name of the column of the home feature we want to compute the correlation of biserial point
+
+        away_feature_column_name (str): Name of the column of the away feature we want to compute the correlation of biserial point
+        
+        k (float): Value used to compute the limits of outliers. We will multiply the IQR by k to compute lower and upper bound for outliers. If we don't want outliers elimination we just need to put a big value for k (like 999).
+        
+        min_week_game (int): The min Game Week of the match we will select to compte the correlation.
+        
+        date_min (datetime): If we want to select only the matchs that played out after a certain date, refer the date with this parameter.
+        
+        dataset_0 (DataFrame): 
+    
+    Returns:
+        
+    """
+    correlation, p_value, mean_ra1, mean_ra0, df_without_outliers_0 = calculcate_feature_correlation( home_feature_column_name, away_feature_column_name, k, min_week_game, date_min, dataset_0)
     
     # Affichage des résultats
-    print(f"Corrélation de point bisérial : {correlation}")
+    print(f"Corrélation de point bisérial pour {home_feature_column_name}: {correlation:.5f}")
     print(f"Valeur de p : {p_value}")
     print("Moyenne feature pour Resultat = 1: ", mean_ra1)
     print("Moyenne feature pour Resultat = 0: ", mean_ra0)
