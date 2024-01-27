e1bb0616f9d50a91b449974f8e9876c77ae43614
luc <pololuc01@gmail.com>
2024-01-27 10:54:42 +0100

    Finish pipeline.results module

--- src/pipeline/results.py
+++ src/pipeline/results.py
@@ -3,6 +3,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.calibration import CalibrationDisplay
+
+# --------------------------------------------------------------
+# GridSearchCV() Results
+# --------------------------------------------------------------
+
 
 # Displaying the results of the GridSearchCV() function that optimised pipeline parameters
 def GridSearchCV_results(grid_search_0, X_train_0):
@@ -33,76 +39,95 @@ def GridSearchCV_results(grid_search_0, X_train_0):
     print("\n\nFeatures selected:",selected_feature_names)
 
 
+# --------------------------------------------------------------
+# Calibration Results
+# --------------------------------------------------------------
 
-# Displaying the results of the model Calibration
-def Calibration_results(CalibrationDisplay_non_calibrated, CalibrationDisplay_calibrated, X_valid_0, X_test_0):
-     """  
-        Present the results of the model calibration. Display 
-        - The calibration curves for the model before calibration and after
-        - The size of datasets used to train and test the calibrator 
-        - A table with the numerical data of the calibration curve of the calibrated model
-        - The average deviation of probabilities for our calibrated model.
-    
+# Plot calibration curves of calibrated and not calibrated pipeline/model
+def plot_calibration_curve(pipeline_0, X_test_0, Y_test_0, n_bins_0, strategy_0, color_0, calibrated_model_or_not):
+    """  
+        Display the annotated calibration curves either for the non calibrated pipeline or for the calibrated one.
+     
      Args:
-        CalibrationDisplay_non_calibrated (sklearn.calibration.CalibrationDisplay): The figure of the calibration curve on X_test of the non calibrated model.
+        pipeline_0 (pipeline): The calibrated or non calibrated pipeline we want to plot the calibration curve of.
         
-        CalibrationDisplay_calibrated (sklearn.calibration.CalibrationDisplay): The figure of the calibration curve on X_test of the calibrated model.
+        X_test_0 (DataFrame): The features Dataframe used to plot the calibration curve.
+        
+        Y_test_0 (DataFrame): The labels/targets Dataframe used to plot the calibration curve.
+        
+        n_bins_0 (int): Number of bins to discretize the predicted probabilities.
+        
+        strategy_0 (str): strategy to discretize the probabilities interval to define the bins intervals. Etither 'uniform' or 'quantile'
+        
+        color_0 (str): Color for plotting the calibration curve. Blue for the calibrated model and Red for non calibrated one.
+        
+        calibrated_model_or_not (Boolean): Wether the pipeline inputed is the calibrated one or not (used to define graph anotations)
+    
+     Returns:
+        sklearn.calibration.CalibrationDisplay : The figure of calibration curve of pipeline_0
+     """
+    Calibration_disp = CalibrationDisplay.from_estimator(pipeline_0, X_test_0, Y_test_0, n_bins = n_bins_0, strategy =strategy_0, color= color_0)
+    # Add labels, legend, and grid
+    if calibrated_model_or_not == True:
+        plt.title('Calibration Curve for calibrated Pipeline')
+    else:
+        plt.title('Calibration Curve for non calibrated Pipeline')
+    plt.xlabel('Mean Predicted Probability')
+    plt.ylabel('Fraction of Positives')
+    if calibrated_model_or_not == True:
+        plt.legend(['Perfectly calibrated', 'Calibrated pipe calibrat° curve'], loc='best')
+    else:
+        plt.legend(['Perfectly calibrated', 'Non calibrated pipe calibrat° curve'], loc='best')
+    plt.minorticks_on() 
+    plt.grid(linewidth=0.5, which ='minor')
+    plt.grid(linewidth=0.9)
+    plt.show() 
+    
+    return Calibration_disp
+
+# Print the statistics of the calibrated pipeline
+def print_calibration_stats(CalibrationDisplay_0, X_test_0, X_valid_0):
+    """
+        Present the stats related to the calibrated pipeline calibration:
+        - The size of datasets used to train the calibrator and test the pipeline
+        - A table with the numerical data of the pipeline calibration curve (The coordinates of the points on the graph) 
+        - The average deviation of probabilities for our pipeline.
+    
+    Args:
+        CalibrationDisplay_0 (sklearn.calibration.CalibrationDisplay): The figure of the calibration curve on X_test of the calibrated pipeline.
         
         X_valid_0 (DataFrame): The features Dataframe we used to train the calibrator. We use it to display its size (nb of rows).
         
-        X_test_0 (DataFrame): The features Dataframe we used to test the calibrated model. We use it to display its size (nb of rows).
+        X_test_0 (DataFrame): The features Dataframe we used to test the calibrator. We use it to display its size (nb of rows)
     
-     Returns:
+    Returns:
         None
-     """
-    
-     # Plot the non calibrated model calibration curve
-     CalibrationDisplay_non_calibrated.plot()
-     # Add labels, legend, and grid
-     plt.title('Calibration Curve for non calibrated Pipeline')
-     plt.xlabel('Mean Predicted Probability')
-     plt.ylabel('Fraction of Positives')
-     plt.legend(['Perfectly calibrated', 'Non calibrated pipe calibrat° curve'], loc='best')
-     plt.minorticks_on() 
-     plt.grid(linewidth=0.5, which ='minor')
-     plt.grid(linewidth=0.9)
-     plt.show() 
-
-
-     # Plot the calibrated model calibration curve
-     CalibrationDisplay_calibrated.plot()
-     # Customize the plot
-     plt.xlabel('Mean Predicted Probability')
-     plt.ylabel('Fraction of Positives')
-     plt.title('Calibration Curve for calibrated pipeline')
-     plt.legend(['Perfectly calibrated', 'Calibrated pipe calibrat° curve'], loc='best')
-     plt.minorticks_on() 
-     plt.grid(linewidth=0.5, which ='minor')
-     plt.grid(linewidth=0.9)
-     plt.show() 
+    """
+    #On calcule et affiche, pour le model calibré, le ratio deviation (moyenne des différence entre prob_pred and prob_true)
+    # Calcul des différences terme à terme des proba predicted and true
+    differences = np.abs(CalibrationDisplay_0.prob_pred - CalibrationDisplay_0.prob_true)
  
+    # Calcul de la moyenne des valeurs absolues des différences qui constituent la deviation
+    deviation = np.mean(differences)
  
-     #On calcule et affiche, pour le model calibré, le ratio deviation (moyenne des différence entre prob_pred and prob_true)
-     # Calcul des différences terme à terme des proba predicted and true
-     differences = np.abs(CalibrationDisplay_calibrated.prob_pred - CalibrationDisplay_calibrated.prob_true)
- 
-     # Calcul de la moyenne des valeurs absolues des différences qui constitue la deviation
-     deviation = np.mean(differences)
- 
-     # Créer le tableau qui contiendra les ratiaux de deviation ainsi que prob_true
-     table_data = {
-         'Proba True': np.round(CalibrationDisplay_calibrated.prob_true, decimals=3),
+    # Créer le tableau qui contiendra les ratiaux de deviation ainsi que prob_true
+    table_data = {
+         'Proba True': np.round(CalibrationDisplay_0.prob_true, decimals=3),
          'Diff with calibrated pred proba': np.round(differences, decimals=3),
      }
- 
-     calibration_df = pd.DataFrame(table_data)
+    
+    #Convert table_data into a DataFrame
+    calibration_df = pd.DataFrame(table_data)
+    
+    # Trier le tableau par ordre décroissant selon la colonne Proba True
+    calibration_df = calibration_df.sort_values(by='Proba True', ascending=False)
 
-     #On afffiche la taille du train_set du calibrateur et du test_set sur lequel on a testé notre model calibré
-     print('train_set size of the calibrator : ', X_valid_0.shape[0])
-     print('test_set size used to test the model-calibrator (nb de matchs sur lequel on a réalisé les calibr curves) : ', X_test_0.shape[0])
- 
-     # Afficher le tableau 
-     print(calibration_df)
- 
-     print('La deviation moyenne pour ce paramétrage est de ', round(deviation*100, 2), "%")
-     print('Le nombre de matchs sur lequel on a réalisé les tests de calibrage est:', X_test_0.shape[0])
+    #On affiche la taille du train_set du calibrateur et du test_set sur lequel on a testé notre model calibré
+    print('Train_set size of the calibrator : ', X_valid_0.shape[0])
+    print('Test_set size used to test the pipeline-calibrator (nb de matchs sur lequel on a réalisé les calibr curves) : ', X_test_0.shape[0], '\n')
+
+    # Afficher le tableau 
+    print(calibration_df)
+
+    print('\nLa deviation moyenne pour ce paramétrage est de ', round(deviation*100, 2), "%")
+    print('Le nombre de matchs sur lequel on a réalisé les tests de calibrage est:', X_test_0.shape[0])
