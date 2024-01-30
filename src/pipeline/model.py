3103f0b64d6a93a6899c8b70994a44e40adeb959
luc <pololuc01@gmail.com>
2024-01-30 22:48:35 +0100

    add features_selected_01

--- src/pipeline/model.py
+++ src/pipeline/model.py
@@ -12,8 +12,13 @@ from sklearn.pipeline import Pipeline
 # --------------------------------------------------------------
 scaler = StandardScaler()
 
-filter_feat_selector = SelectKBest(f_classif, k= 16)
-
 model_01 = LogisticRegression(penalty = 'l2', C = 0.0486967525, fit_intercept=True, random_state = 999, solver = 'sag', class_weight = {1:0.6, 0:0.4}, max_iter= 3000, verbose = False, tol=1e-4)
 
-pipeline_01 = Pipeline(steps=[ ("scaler", scaler), ("features_selector",filter_feat_selector), ("model", model_01)])
+features_selected_01 =['Avg_victory', 'Avg_points_pm_ponderated_by_adversary_perf',
+       'Avg_goal_diff', 'Avg_scored_g_conceeded_g_ratio',
+       'Avg_collected_points', 'Week_ranking', 'Annual_budget', 'Points_5lm',
+       'Goal_Diff_5lm', 'Week_ranking_5lm', 'Diff_avg_corners_nb',
+       'Avg_shots_nb', 'Avg_shots_on_target_nb', 'Avg_possession',
+       'Avg_odds_victory_proba', 'H_A_status']
+
+pipeline_01 = Pipeline(steps=[ ("scaler", scaler),  ("model", model_01)])
