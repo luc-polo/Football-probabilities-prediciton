115d5e248c46b698a2ba18bcb634820ebbde79f4
luc <pololuc01@gmail.com>
2024-01-30 10:23:14 +0100

    implementation of data_combination.find_corresponding_match()

--- src/data/data_combination.py
+++ src/data/data_combination.py
@@ -5,7 +5,13 @@
 # 
 # --------------------------------------------------------------
 def team_names_uniformisation(Football_data_dataset_0, Footy_Stats_dataset_0):
-    """Uniformise the names of teams in Footy Stats and Football Data datasets
+    """
+        Uniformise the names of teams in Footy Stats and Football Data datasets
+    
+    Args:
+        Football_data_dataset_0 (DataFrame): Dataframe containing data from Football Data website
+        
+        Footy_Stats_dataset_0 (DataFrame): Dataframe containing data from Footy Data website
     """
     # Change Football Data team names
     football_datat_team_names_to_change = {'Ajaccio GFCO' : 'Gazélec Ajaccio',
@@ -31,9 +37,25 @@ def team_names_uniformisation(Football_data_dataset_0, Footy_Stats_dataset_0):
     return Football_data_dataset_0, Footy_Stats_dataset_0
 
 
-def find_corresponding_match():
-    """Find the corresponding match in Football Data dataset
+def find_corresponding_match(row_nb_in_footy_dataset, footy_dataset_0, football_data_dataset_0):
+    """Find the corresponding match in Football Data dataset and return its index
     """
+    date_of_the_match = footy_dataset_0.at[row_nb_in_footy_dataset,'date_GMT'].date()
+    
+    dat_col_of_footaball_dataset = football_data_dataset_0['Date'].dt.date
+    
+    # We identify all rows in football_data_dataset_0 that have the same date as the match of footy dataset we are looking for
+    possible_rows_identified_with_dates = football_data_dataset_0[dat_col_of_footaball_dataset == date_of_the_match]
+    
+    row_nb_in_football_data_dataset = 0
+    
+    for index, row in possible_rows_identified_with_dates.iterrows():
+        if row['HomeTeam'] == footy_dataset_0.at[row_nb_in_footy_dataset, 'home_team_name'] and row['AwayTeam'] == footy_dataset_0.at[row_nb_in_footy_dataset, 'away_team_name']:
+            row_nb_in_football_data_dataset = index
+            break   # Exit the loop once a match is found
+    
+    return row_nb_in_football_data_dataset
+    
 
 def replace_col_values():
     """Replace values in the HT and AT columns of Footy Stats dataset by the values in Football Data values
