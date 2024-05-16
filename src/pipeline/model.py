18ebb3a954e0a9d000ecdbe320c996339c7f932b
luc <pololuc01@gmail.com>
2024-05-16 18:02:24 +0200

    created save_pipeline()

--- src/pipeline/model.py
+++ src/pipeline/model.py
@@ -5,7 +5,8 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
 from sklearn.preprocessing import RobustScaler, StandardScaler
 from sklearn.pipeline import Pipeline
-
+import os
+import joblib
 
 # --------------------------------------------------------------
 # Pipeline 1
@@ -88,3 +89,39 @@ pipeline_03 = Pipeline(steps=[('scaler', StandardScaler()),
        'Goal_Diff_5lm', 'Week_ranking_5lm', 'Diff_avg_corners_nb',
        'Avg_shots_nb', 'Avg_shots_on_target_nb', 'Avg_possession',
        'Avg_odds_victory_proba', 'H_A_status']
+
+
+# --------------------------------------------------------------
+# Saving a pipeline function
+# -------------------------------------------------------------- 
+
+def save_pipeline(pipeline_0, pipeline_name_0):
+       """_summary_
+
+       Args:
+           pipeline_0 (_type_): _description_
+           pipeline_name_0 (_type_): _description_
+       """
+       
+       # defining the destination path of the pipeline
+       pipeline_destination_path = f"C:/Users/polol/OneDrive/Documents/ML/Projet Mbappe (11.23- )/Projet Mbappe Cookiestructure/models/{pipeline_name_0}.pkl"
+       
+       # Delete the old pipeline if it exists
+       try:
+              os.remove(pipeline_destination_path)
+              print(f"Successfully deleted the old pipeline:     {pipeline_name_0}")
+       except FileNotFoundError:
+              print(f"No old pipeline to delete at:              {pipeline_name_0}")
+       except Exception as e:
+              print(f"An error occurred while deleting the old pipeline: {e}")
+              
+       # Save the new dataframe
+       try:
+              joblib.dump(pipeline_0, pipeline_destination_path)
+              print(f"Successfully saved the new pipeline:       {pipeline_name_0}")
+       except Exception as e:
+              print(f"An error occurred while saving the new pipeline: {e}")
+       
+       #Put a line break before at the end of the text w've just printed
+       print("\n")
+       
