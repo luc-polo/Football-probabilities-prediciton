c51ad4f449d7d67814a21abe6cb7e5cc72911f25
Luc POLO <pololuc01@gmail.com>
2025-01-04 12:16:21 +0100

    ras

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description="You're desperatly looking for something like 4 Milli to buy a brand new Maybach to turns into a chicks magnet? Moreover you plan to buy gta6 next year? Hmmm, let me think it over a few sec... I belive this project will helps you a lot!",
+    description="",
     author='Luc POLO',
     license='',
 )
